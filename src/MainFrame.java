6be9e8e71460466dcf074abbb7177920a3ba21c8
chenchianstudent <105021007@live.asia.edu.tw>
2017-09-19 11:50:28 +0800

    02

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,8 +1,11 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
+    private Button bt1 = new Button("Exit");
 
     public MainFrame() {
         init();
@@ -10,12 +13,21 @@ public class MainFrame extends Frame {
 
     private void init() {
         this.setBounds(100, 120, 500, 400);
+        this.setLayout(null);
+        bt1.setBounds(50, 60, 100, 100);
+        bt1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        this.add(bt1);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-        
+
     }
 }
